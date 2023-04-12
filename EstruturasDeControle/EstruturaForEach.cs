ab00db4e913fb37ad02cf0378e02992068b31548
zennom <delimaviviane@yahoo.com.br>
2023-04-12 09:45:55 -0300

    foreach

--- EstruturasDeControle/EstruturaForEach.cs
+++ EstruturasDeControle/EstruturaForEach.cs
@@ -8,6 +8,19 @@ namespace CursoCSharp.EstruturasDeControle {
     internal class EstruturaForEach {
         public static void Executar() {
 
+            //percorrendo uma palavra
+            var palavra = "Olá!";
+
+            foreach(var letra in palavra) {
+                Console.WriteLine(letra);
+            }
+
+            //percorrendo um array
+            var alunos = new string[] { "Ana", "Bia", "João" };
+            foreach(string aluno in alunos) {
+                Console.WriteLine(aluno);
+            }
+
         }
 
     }
